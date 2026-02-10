15369a8c66fff0c4e140e27c03c19109e3b81a41
Logan Miller <46401738+Loganocm@users.noreply.github.com>
2026-02-10 11:26:19 -0500

    Update MenuScene.ts

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,6 +1,6 @@
 import { Container } from 'pixi.js';
 import type { IScene } from '../core/SceneManager';
-import { UIManager } from '../ui/UIManager';
+
 
 export class MenuScene implements IScene {
   container: Container;
